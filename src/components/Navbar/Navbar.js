c9177ec957e6d7ce91eea6cee3be51fe88f528d8
Vladimir Tesiolchin <vladimir@MacBook-Pro-Vladimir.local>
2020-09-10 10:47:31 -0700

    Created dropdown menu for pages

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -66,5 +66,4 @@ const Navbar = () => {
 	}
 
 
-
 export default Navbar;
--- src/components/Navbar/Navbarpages.js
+++ src/components/Navbar/Navbarpages.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../../logo-color.svg';
 import cart from '../../cart-pages.svg';
 import '../../App.css';
+import DropdownMan from './DropdownMan';
+import DropdownWomen from './DropdownWomen';
 
-class Navbarpages extends React.Component {
-	render() {
+const Navbarpages = () => {
+	const [visible, setVisible,] = useState(false)
+	const [vis, setVis,] = useState(false)
+
+    const displayMenu = () => {
+        setVisible(true)
+    }
+    const hideMenu = () => {
+        setVisible(false)
+	}
+	const display = () => {
+        setVis(true)
+    }
+    const hide = () => {
+        setVis(false)
+	}
 		return (
 			<div className='container'>
 				<div className='row'>
@@ -15,17 +31,27 @@ class Navbarpages extends React.Component {
 								<img src={logo} alt='brand' className='brand-logo' />
 							</Link>
 							<ul className='navbar-nav align-items-center mx-auto' >
-								<li className="nav-item ml-5">
-									<Link to='/man' className='nav-link-pages'>Man</Link>
+								<li 
+									className="nav-item ml-5"
+									onMouseLeave={hideMenu}>
+									<Link to='/man' 
+										className='nav-link pages'
+										onMouseOver={displayMenu}
+										>Man</Link>
+										<DropdownMan isVisible={visible} />
 								</li>
-								<li className="nav-item ml-5">
-									<Link to='/women' className='nav-link-pages'>Women</Link>
+								<li 
+									className="nav-item ml-5"
+									onMouseLeave={hide}>
+									<Link to='/women' 
+										className='nav-link pages'
+										onMouseOver={display}
+										>Women</Link>
+										<DropdownWomen isVisible={vis} />
 								</li>
+							
 								<li className="nav-item ml-5">
-									<Link to='/kids' className='nav-link-pages'>Kids</Link>
-								</li>
-								<li className="nav-item ml-5">
-									<Link to='/sale' className='nav-link-pages'>Sale</Link>
+									<Link to='/sale' className='nav-link pages'>Sale</Link>
 								</li>
 							</ul>
 							<Link to='/shoppingcart' className='ml-auto'>
@@ -37,7 +63,7 @@ class Navbarpages extends React.Component {
 			</div> 
 		)
 	}
-}
+
 
 
 
