e589be5908cb56f92d9a106bde33aa845ebf612a
Vladimir Tesiolchin <vladimir@MacBook-Pro-Vladimir.local>
2021-05-19 09:25:04 -0700

    Deleted old routing

--- src/components/Routing.js
+++ src/components/Routing.js
@@ -9,41 +9,13 @@ import Sale from "./ProductsPages/Sale";
 import Modal from "./Modal/Modal";
 import CheckoutForm from "./CheckoutForm/CheckoutForm";
 import TableSize from "./Details/TableSize";
+// ********
 import NewArrival from "./ProductsPages/NewArrival";
 import TshirtsMen from "./MenProductPages/TshirtsMen";
-import PoloMen from "./MenProductPages/PoloMen";
-import ShirtsMen from "./MenProductPages/ShirtsMen";
-import SweatersMen from "./MenProductPages/SweatersMen";
-import OverallsMen from "./MenProductPages/OverallsMen";
-import JacketMen from "./MenProductPages/JacketMen";
-import HoodiesMen from "./MenProductPages/HoodiesMen";
-import ActivewearMen from "./MenProductPages/ActivewearMen";
-import JeansMen from "./MenProductPages/JeansMen";
-import ShortsMen from "./MenProductPages/ShortsMen";
-import PantsMen from "./MenProductPages/PantsMen";
-import JoggersMen from "./MenProductPages/JoggersMen";
 import NewArrivedMen from "./MenProductPages/NewArrivedMen";
-import SuitesMen from "./MenProductPages/SuitesMen";
-import ShoesMen from "./MenProductPages/ShoesMen";
-import UnderwearMen from "./MenProductPages/UnderwearMen";
 import SaleMen from "./MenProductPages/SaleMen";
-import TshirtsWomen from "./WomenProductPages/TshirtsWomen";
-import Dresses from "./WomenProductPages/Dresses";
-import JumpsuitsWomen from "./WomenProductPages/JumpsuitsWomen";
-import LoungewearWomen from "./WomenProductPages/LoungewearWomen";
-import CoatsWomen from "./WomenProductPages/CoatsWomen";
-import CardigansWomen from "./WomenProductPages/CardigansWomen";
-import HoodiesWomen from "./WomenProductPages/HoodiesWomen";
-import SwimwearWomen from "./WomenProductPages/SwimwearWomen";
-import JeansWomen from "./WomenProductPages/JeansWomen";
-import SkirtsWomen from "./WomenProductPages/SkirtsWomen";
-import ShortsWomen from "./WomenProductPages/ShortsWomen";
-import PantsWomen from "./WomenProductPages/PantsWomen";
 import SaleWomen from "./WomenProductPages/SaleWomen";
 import NewArrivedWomen from "./WomenProductPages/NewArrivedWomen";
-import SuitesWomen from "./WomenProductPages/SuitesWomen";
-import ShoesWomen from "./WomenProductPages/ShoesWomen";
-import UnderwearWomen from "./WomenProductPages/UnderwearWomen";
 
 import CategoryDetails from "./MainProducts/CategoryDetails";
 
@@ -61,23 +33,8 @@ function Routing() {
 
         {/* Women's Pages */}
 
-        <Route path="/women's-t-shirts" component={TshirtsWomen} />
-        <Route path="/women's-t-dresses" component={Dresses} />
-        <Route path="/women's-t-jumpsuits" component={JumpsuitsWomen} />
-        <Route path="/women's-t-loungewear" component={LoungewearWomen} />
-        <Route path="/women's-t-coats" component={CoatsWomen} />
-        <Route path="/women's-t-cardigans" component={CardigansWomen} />
-        <Route path="/women's-t-hoodies" component={HoodiesWomen} />
-        <Route path="/women's-t-swimwear" component={SwimwearWomen} />
-        <Route path="/women's-t-jeans" component={JeansWomen} />
-        <Route path="/women's-t-skirts" component={SkirtsWomen} />
-        <Route path="/women's-t-shorts" component={ShortsWomen} />
-        <Route path="/women's-t-paints" component={PantsWomen} />
         <Route path="/women's-sale" component={SaleWomen} />
         <Route path="/women's-new-arrived" component={NewArrivedWomen} />
-        <Route path="/women's-suites" component={SuitesWomen} />
-        <Route path="/women's-shoes" component={ShoesWomen} />
-        <Route path="/women's-lingerie" component={UnderwearWomen} />
 
         <Route path="/:id" component={CategoryDetails} />
 
