c7ac7fbb85b9741c4f4528c236070bfd5a83705e
Vladimir Tesiolchin <vladimir@MacBook-Pro-Vladimir.local>
2021-05-21 09:13:48 -0700

    Remove unnecessary components with men's products

--- src/components/MenProductPages/TshirtsMen.js
+++ src/components/MenProductPages/TshirtsMen.js
@@ -1,51 +1,51 @@
-import React from 'react';
-import Title from '../Title/Title';
-import Summer from '../Summer/Summer';
-import Footer from '../Footer/Footer';
-import { ProductConsumer } from '../../context';
-import Card from '../Card/Card';
-import Navbar from '../Navbar/Navbarpages';
-import Breadcrumbs from '../Breadcrumbs/Breadcrumbs';
-import Drawer from '../Navbar/Drawer';
+import React from "react";
+import Title from "../Title/Title";
+import Summer from "../Summer/Summer";
+import Footer from "../Footer/Footer";
+import { ProductConsumer } from "../../context";
+import Card from "../Card/Card";
+import Navbar from "../Navbar/Navbarpages";
+import Breadcrumbs from "../Breadcrumbs/Breadcrumbs";
+import Drawer from "../Navbar/Drawer";
 
+class TshirtsMen extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Navbar />
+        <Drawer />
+        <div className="container products-page">
+          <Breadcrumbs />
+          <div className="row">
+            <div className="col-12 product-page-title mx-auto text-center">
+              <Title name="Men's" title="T-shirts"></Title>
+              <div className="col-12 col-md-10 col-lg-8 mx-auto text-center pages-text">
+                Keep your look fresh when the temperatures start to soar with
+                our collection of men's shorts. You can't go wrong with men's
+                denim shorts – they're a wardrobe staple and can be styled your
+                way. Or try a pair of men's cargo shorts if you're after
+                something practical yet stylish.
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <ProductConsumer>
+              {(value) => {
+                return value.products.map((product) => {
+                  if (product.sex === "men" && product.category === "tshirt") {
+                    return <Card key={product.id} product={product} />;
+                  }
+                });
+              }}
+            </ProductConsumer>
+          </div>
+        </div>
 
-class TshirtsMan extends React.Component {
-	render() {
-		return (
-			<React.Fragment>
-				<Navbar />
-				<Drawer />
-					<div className="container products-page">
-						<Breadcrumbs />
-						<div className='row'>
-							<div className='col-12 product-page-title mx-auto text-center'>
-							<Title name="Men's" title="T-shirts"></Title>
-							<div className='col-12 col-md-10 col-lg-8 mx-auto text-center pages-text'>
-							T-shirts are the ultimate wardrobe staple – they're simple, they're versatile and they'll work for virtually any occasion. 
-							Hit up our collection of men's T-shirts and vests for plain and printed designs to slot into your everyday rotation. 
-							Check out web-site for oversized T-shirts and unique patterns.
-								</div> 
-							</div>
-						</div>
-						<div className="row">
-							<ProductConsumer>
-								{value => {
-									return value.products.map(product => {
-										if (product.sex === 'men' && product.category === 'tshirt') {
-											return <Card key={product.id} product ={product} />;
-										}
-									});
-								}}
-							</ProductConsumer>
-						</div>
-					</div>
-
-		        <Summer />
-		        <Footer />
-      		</React.Fragment>
-
-		)
-	}
+        <Summer />
+        <Footer />
+      </React.Fragment>
+    );
+  }
 }
 
-export default TshirtsMan;
+export default TshirtsMen;
